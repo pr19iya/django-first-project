f662758e5e2f6490ec994475d770aaf717b6d26e
pr19iya <priya3emails@gmail.com>
2025-05-17 21:41:00 +0530

    changes

--- first_project/settings.py
+++ first_project/settings.py
@@ -115,12 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+
 
 
 
